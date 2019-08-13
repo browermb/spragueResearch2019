ca826ba1f0b377063d3472054b0a7a655c9f5e5b
Maddie Brower <browermb@dukes.jmu.edu>
2019-08-13 13:03:12 -0400

    update

--- multiple_dac_test/multiple_dac_test.ino
+++ multiple_dac_test/multiple_dac_test.ino
@@ -4,10 +4,11 @@
 Adafruit_MCP4725 dac1;
 Adafruit_MCP4725 dac2;
 
-int steerRelay = 7;
+int steerRelay = 9;
 int brakeRelay = 8;
 
 void setup() {
+  Wire.begin();
   dac1.begin(0x62);
   dac2.begin(0x63);
   pinMode(steerRelay, OUTPUT);
@@ -16,25 +17,24 @@ void setup() {
   while (!Serial) {
     delay(15);
   }
-   Serial.println("Generating a triangle wave");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-   int counter;
-    // Run through the full 12-bit scale for a triangle wave
     Serial.println("Setting: 0");
     dac1.setVoltage(0, false);
     dac2.setVoltage(0, false);
-    
+    digitalWrite(brakeRelay, LOW);
+    digitalWrite(steerRelay, LOW);
     delay(5000);
+    
     Serial.println("Setting: 2048");
-
+    digitalWrite(brakeRelay, HIGH);
+    digitalWrite(steerRelay, HIGH);
     dac1.setVoltage(2048, false);
     dac2.setVoltage(2048, false);
     delay(5000);
+    
     Serial.println("Setting: 4095");
-
     dac1.setVoltage(4095, false);
     dac2.setVoltage(4095, false);
     delay(5000);
